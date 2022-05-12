0fde8ac55851c6c02ce5a54ef8523e7e41fb0c95
Daniel Elías Torres Gómez <doctorresg@gmail.com>
2022-05-11 23:03:20 -0500

    se agregaron CheckBox y Spinner

--- app/src/main/java/com/example/basicelementsiu/MainActivity.kt
+++ app/src/main/java/com/example/basicelementsiu/MainActivity.kt
@@ -2,10 +2,21 @@ package com.example.basicelementsiu
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val spinner: Spinner = findViewById(R.id.spinner)
+        ArrayAdapter.createFromResource(
+            this,
+            R.array.tipos_documento,
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            spinner.adapter = adapter
+        }
     }
 }
