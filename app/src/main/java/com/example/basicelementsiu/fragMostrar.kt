d0f4a28eab54b59de458cf6c0b7b40df245067a2
Daniel Elías Torres Gómez <doctorresg@gmail.com>
2022-05-17 14:32:41 -0500

    mejoras

--- app/src/main/java/com/example/basicelementsiu/fragMostrar.kt
+++ app/src/main/java/com/example/basicelementsiu/fragMostrar.kt
@@ -22,8 +22,11 @@ class fragMostrar : Fragment(R.layout.fragment_frag_mostrar) {
         val redPreference : SharedPreferences = this.requireContext().getSharedPreferences("datos",
             Context.MODE_PRIVATE)
         val name = redPreference.getString("nombre", "no hay nada guardado")
-        val nombreShow = requireView().findViewById<TextView>(R.id.edtNombre)
+        val nombreShow = requireView().findViewById<TextView>(R.id.tvNombre)
         nombreShow.text = name
+        val nroDocumento =redPreference.getString("documento", "no se ha guardado")
+        val documentShow = requireView().findViewById<TextView>(R.id.tvNroDoc)
+        documentShow.text = nroDocumento
     }
 
     override fun onCreateView(
